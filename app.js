df0e1bc4e1a4270e4a3d9d53d1e730f83ebeb9d0
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-29 17:42:16 +0300

    Kullanıcı girişi için API endpoint eklendi; email ve şifre doğrulama işlemleri gerçekleştirildi.

--- app.js
+++ app.js
@@ -264,6 +264,66 @@ app.post("/register", async(req, res) => {
 }); 
 
 
+app.post("/login", async (req, res) => {
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+
+        if(!email || !password){
+            return res.status(400).json({
+                message: "Email ve Password Gerekli."
+            });
+        }
+
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({
+                message: "Email veya şifre hatalı."
+            });
+        }
+
+        const user = result.rows[0];
+
+        const passwordMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!passwordMatch) {
+            return res.status(401).json({
+                message: "Email veya şifre hatalı."
+            });
+        }
+
+         const token = jwt.sign(
+            {
+                userId: user.id,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        );  
+        res.json({
+            message: "Giriş başarılı.",
+            token: token
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: "Giriş yapılamadı.",
+            error: error.message
+        });
+    }
+});
+
+
 
 app.listen(3000, () => {
     console.log("Server 3000 Portunda Çalışıyor...");
